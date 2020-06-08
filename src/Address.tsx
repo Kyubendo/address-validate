3c41a940e7f542cd46b36f3b5028710a9757817b
Kyubendo <kotigo111@gmail.com>
2020-06-08 17:59:20 +0300

    reducer

--- src/Address.tsx
+++ src/Address.tsx
@@ -3,28 +3,52 @@ import {validateAddress} from "./pages"
 import SelectRegion from "./SelectRegion"
 import SelectTown from "./SelectTown";
 import addressesRaw from "./data/addresses.json";
+import SelectAddress from "./SelectAddress";
+
+type State = {
+    region: string;
+    town: string;
+}
+
+type Action = {
+    field: string;
+    value: string;
+}
+
+type Reducer = React.Reducer<State, Action>;
+type Dispatch = React.Dispatch<Action>;
+
+const Values: {[k in keyof State]: Array<State[k]>} = {
+    region: ['a','b','c'],
+    town: ['1','2','3',],
+}
+
+const Select: React.FC<{
+    dispatch: Dispatch;
+    state: State;
+    field: keyof State;
+}> = ({field, dispatch, state}) => {
+    const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        const value = e.target.value;
+        dispatch({field, value});
+    }
+    return <select name={field} onChange={handleChange} value={state[field]}>
+        {Values[field].map((value) => <option key={value}>{value}</option>)}
+    </select>
+}
 
 const Address = () => {
-    const [region, setRegion] = useState("Харківська")
-    const [town, setTown] = useState("м. Харків")
-    const [value, setValue] = useState('')
+    const [state, dispatch] = React.useReducer<Reducer>((prevState, action) => {
+        return {
+            ...prevState,
+            [action.field]: action.value,
+        };
+    }, {region: '', town:'',});
 
     return (
         <div style={{width: "50%", margin: "0 auto"}}>
-            <SelectRegion region={region} onRegionChange={(e) => {
-                setRegion(e.target.value)
-                setTown(Object.keys(addressesRaw[e.target.value])[0])
-                setValue('')
-            }}/>
-            <br/>
-            <SelectTown region={region} town={town} onTownChange={(e) => {
-                setTown(e.target.value)
-                setValue('')
-            }}/>
-            <br/>
-            <input value={value} type={'text'} onChange={
-                (e) => validateAddress(e.target.value, region, town) && setValue(e.target.value)}
-            />
+            <Select dispatch={dispatch} state={state} field="region" />
+            <Select dispatch={dispatch} state={state} field="town" />
         </div>
     );
 }
